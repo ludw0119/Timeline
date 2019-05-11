2f0fbb99c9456d78eae97f5c09488b3eb98fde9a
ludw0119 <ludw0119@stud.kea.dk>
2019-05-11 11:06:13 +0200

    white ellipses animation added

--- timeline.js
+++ timeline.js
@@ -11,72 +11,31 @@ function loadSVG() {
       document
         .querySelector("#svg_timeline")
         .insertAdjacentHTML("afterbegin", svgdata);
+
+      let whiteEllipse = document.querySelectorAll(".cls-41");
+
+      //console.log(whiteEllipse);
+
+      function stoneBigger() {
+        whiteEllipse.forEach(element => {
+          element.addEventListener("mouseover", whiteEllipseBigger);
+          element.addEventListener("mouseout", whiteEllipseSmaller);
+        });
+      }
+
+      function whiteEllipseBigger() {
+        this.classList.add("stoneBigger");
+        //alert("mouseover");
+      }
+
+      function whiteEllipseSmaller() {
+        if (this.classList.contains("stoneBigger")) {
+          //setTimeout(function() {
+          this.classList.remove("stoneBigger");
+          //}, 3000);
+        }
+      }
+
+      stoneBigger();
     });
-  //setTimeout(position, 1000);
-  setTimeout(datesPosition, 1000);
-  setTimeout(placesPosition, 1000);
-  //setTimeout(fitText("#whiteElipses #whiteEl10", "#divText1"), 3000);
-}
-
-/*function fitText(svgElementSource, htmlElementSource) {
-  svgElement = document.querySelector(svgElementSource);
-  htmlElement = document.querySelector(htmlElementSource);
-
-  console.log(svgElement);
-  htmlElement.style.left = svgElement.getAttribute("x") + "px"; //this "+px" is a string concatenation to get a nr of pixels in a right syntax
-  htmlElement.style.top = svgElement.getAttribute("y") + "px";
-  htmlElement.style.width = svgElement.getAttribute("width") + "px";
-  htmlElement.style.height = svgElement.getAttribute("height") + "px";
-}*/
-
-/*function position() {
-  const elipse = document.querySelector("#Bubble1");
-  const rect = elipse.getBoundingClientRect(); //The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-
-  const htmlElement = document.querySelector("#divText1");
-  htmlElement.style.left = rect.left + 10 + "px"; //this "+px" is a string concatenation to get a nr of pixels in a right syntax
-  htmlElement.style.top = rect.top + 20 + "px";
-  htmlElement.style.color = "pink";
-  htmlElement.style.position = "absolute";
-}*/
-
-function datesPosition() {
-  const ellipses = document.querySelectorAll(".bubbles");
-
-  for (let i = 1; i <= ellipses.length; i++) {
-    let bubbleId = "#Bubble" + i;
-    let divId = "#divText" + i;
-    let elipse = document.querySelector(bubbleId);
-    let rect = elipse.getBoundingClientRect(); //The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-
-    let htmlElement = document.querySelector(divId);
-    htmlElement.style.left = rect.left + 5 + "px"; //this "+px" is a string concatenation to get a nr of pixels in a right syntax
-    htmlElement.style.top = rect.top + 20 + "px";
-    htmlElement.style.position = "absolute";
-  }
-}
-
-function placesPosition() {
-  const ellipses = document.querySelectorAll(".bubbles");
-
-  for (let i = 1; i <= ellipses.length; i++) {
-    let bubbleId = "#Bubble" + i;
-    //let divId = "#divText" + i;
-    let divPlaceId = "#divPlaceText" + i;
-    let elipse = document.querySelector(bubbleId);
-    let rect = elipse.getBoundingClientRect(); //The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-
-    let htmlElement2 = document.querySelector(divPlaceId);
-    if (i % 2 == 0) {
-      //left bubble
-      htmlElement2.style.left = rect.left + 165 + "px"; //this "+px" is a string concatenation to get a nr of pixels in a right syntax
-      htmlElement2.style.top = rect.top + 20 + "px";
-      htmlElement2.style.position = "absolute";
-    } else {
-      //right bubble
-      htmlElement2.style.left = rect.left - 165 + "px"; //this "+px" is a string concatenation to get a nr of pixels in a right syntax
-      htmlElement2.style.top = rect.top + 20 + "px";
-      htmlElement2.style.position = "absolute";
-    }
-  }
 }
