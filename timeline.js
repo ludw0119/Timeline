b02f32416d9739c847a0776f80a4b1e59b4dcaa8
ludw0119 <ludw0119@stud.kea.dk>
2019-05-12 10:10:40 +0200

    white ellipses animation cleaned up

--- timeline.js
+++ timeline.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", loadSVG);
 function loadSVG() {
   //console.log("Load the SVG");
 
-  fetch("CV-timeline-color.svg")
+  fetch("CV-timeline-color3.svg")
     .then(response => response.text())
     .then(svgdata => {
       //console.log("The SVG data is:");
@@ -12,30 +12,55 @@ function loadSVG() {
         .querySelector("#svg_timeline")
         .insertAdjacentHTML("afterbegin", svgdata);
 
-      let whiteEllipse = document.querySelectorAll(".cls-41");
-
+      let whiteEllipse = document.querySelectorAll(".cls-28");
+      let textPlace = document.querySelectorAll(".cls-29");
       //console.log(whiteEllipse);
 
-      function stoneBigger() {
+      function stoneGrey() {
         whiteEllipse.forEach(element => {
-          element.addEventListener("mouseover", whiteEllipseBigger);
-          element.addEventListener("mouseout", whiteEllipseSmaller);
+          element.addEventListener("mouseover", whiteEllipseGrey);
+          //element.addEventListener("mouseover", functionalert);
+          element.addEventListener("mouseout", whiteEllipseGreyOff);
         });
       }
 
-      function whiteEllipseBigger() {
-        this.classList.add("stoneBigger");
-        //alert("mouseover");
+      /*function functionalert() {
+        alert("this is alert");
+      }*/
+
+      function whiteEllipseGrey() {
+        this.classList.add("stoneGrey");
+        //alert("mouseover")
       }
 
-      function whiteEllipseSmaller() {
-        if (this.classList.contains("stoneBigger")) {
+      function whiteEllipseGreyOff() {
+        if (this.classList.contains("stoneGrey")) {
           //setTimeout(function() {
-          this.classList.remove("stoneBigger");
+          this.classList.remove("stoneGrey");
           //}, 3000);
         }
       }
 
-      stoneBigger();
+      function textChangeEllipse() {
+        textPlace.forEach(element => {
+          element.addEventListener("mouseover", txtHoverEllipseGrey);
+        });
+      }
+
+      function txtHoverEllipseGrey() {
+        //alert("mouseover");
+        //console.log(this.id);
+        let textItem = this.id;
+        let ellipse = "#" + textItem.replace("Place", "whiteEl");
+        let ellipseObject = document.querySelector(ellipse);
+        //textObject = document.querySelector(textItem);
+        //console.log("text" + textObject);
+        ellipseObject.classList.add("stoneGrey");
+
+        this.style.color = "pink";
+      }
+
+      stoneGrey();
+      textChangeEllipse();
     });
 }
